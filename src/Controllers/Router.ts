2748bc1bbabb348007563765b2e3a7f630518315
Victor <victorgutierrezgomes@gmail.com>
2020-08-25 16:26:34 -0400

    Chore: Implement Routes Controller

---
+++ src/Controllers/Router.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+
+class Router {
+    signUp(req: Request, res: Response) {}
+
+    login(req: Request, res: Response) {}
+
+    doFriendship(req: Request, res: Response) {}
+
+    undoFriendship(req: Request, res: Response) {}
+
+    createPost(req: Request, res: Response) {}
+
+    getFeed(req: Request, res: Response) {}
+
+    getFeedByyType(req: Request, res: Response) {}
+
+    getFeedByPage(req: Request, res: Response) {}
+
+    likePost(req: Request, res: Response) {}
+
+    unlikePost(req: Request, res: Response) {}
+
+    commentPost(req: Request, res: Response) {}
+
+    refreshUserToken(req: Request, res: Response) {}
+}
