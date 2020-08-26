e0df454f139a4d39eaacdcc6a6132c798d8d83c6
Victor <victorgutierrezgomes@gmail.com>
2020-08-26 12:32:28 -0400

    Refactor: make use of UserDTO

--- src/Data/UserDatabase.ts
+++ src/Data/UserDatabase.ts
@@ -1,14 +1,15 @@
+import { UserDTO } from '../Types';
 import { BaseDatabase } from './BaseDatabase';
 
 class UserDatabase extends BaseDatabase {
     private static TABLE_NAME = 'Users';
 
-    public async createUser(
-        id: string,
-        email: string,
-        name: string,
-        password: string
-    ): Promise<void> {
+    public async createUser({
+        id,
+        email,
+        name,
+        password,
+    }: UserDTO): Promise<void> {
         try {
             await this.getConnection()
                 .insert({
@@ -19,24 +20,23 @@ class UserDatabase extends BaseDatabase {
                 })
                 .into(UserDatabase.TABLE_NAME);
         } catch (error) {
-            // SQL MESSAGE INCLUDES THE 'DUPLICATE ENTRY' SENTENCE WHEN
-            // UNIQUE KEY EMAIL IS ATTEMPTED TO BE RECREATED WITH ALREADY EXISTENT VALUE
             if (error.message.includes('Duplicate entry'))
                 throw new Error('This user already exists');
         }
     }
 
-    public async getUserByEmail(email: string) {
+    public async getUserByEmail({ email }: Partial<UserDTO>) {
         try {
             const result = await this.getConnection()
                 .select('*')
                 .from(UserDatabase.TABLE_NAME)
                 .where({ email });
 
-            if (result[0] === undefined)
+            if (result[0] === undefined) {
                 throw new Error('This user does not exist');
-
-            return result[0];
+            } else {
+                return result[0];
+            }
         } catch (error) {
             throw error;
         }
