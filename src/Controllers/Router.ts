221fbca7deb91786c8bad748f7bb18e4aa96db6b
Victor <victorgutierrezgomes@gmail.com>
2020-08-26 12:33:12 -0400

    Refactor: make use of User Model

--- src/Controllers/Router.ts
+++ src/Controllers/Router.ts
@@ -4,6 +4,7 @@ import Authenticator from '../Services/Authenticator';
 import HashManager from '../Services/HashManager';
 import IdGenerator from '../Services/IdGenerator';
 import ParamChecker from '../Services/ParamChecker';
+import User from '../Models/User';
 
 class Router {
     async signUp(req: Request, res: Response): Promise<void> {
@@ -18,7 +19,9 @@ class Router {
 
             const hashPassword = await HashManager.hash(password);
 
-            await UserDatabase.createUser(id, email, name, hashPassword);
+            await UserDatabase.createUser(
+                new User(id, email, name, hashPassword)
+            );
 
             const token = Authenticator.generateToken({ id });
 
@@ -27,9 +30,15 @@ class Router {
                 token,
             });
         } catch (error) {
-            res.status(400).send({
-                message: error.message,
-            });
+            if (error.message === 'This user already exists') {
+                res.status(409).send({
+                    message: error.message,
+                });
+            } else {
+                res.status(400).send({
+                    message: error.message,
+                });
+            }
         }
     }
 
@@ -42,16 +51,16 @@ class Router {
 
             await HashManager.compare(password, user.password);
 
-            const token = await Authenticator.generateToken({ id: user.id });
+            const token = Authenticator.generateToken({ id: user.id });
 
             res.status(200).send({
                 message: 'User logged successfully',
                 token,
             });
         } catch (error) {
-            if (error.message === '0001') {
+            if (error.message === 'NO MATCH') {
                 res.status(401).send({
-                    message: 'Password is incorrect',
+                    message: 'User or password is incorrect',
                 });
             } else {
                 res.status(400).send({
