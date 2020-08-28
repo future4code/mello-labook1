c7d8da890c5bf5b9b7b31bcf1e7d87340ad691e2
Victor <victorgutierrezgomes@gmail.com>
2020-08-27 20:25:12 -0400

    Feature: implement dateFormat checker in ParamChecker

--- src/Services/ParamChecker.ts
+++ src/Services/ParamChecker.ts
@@ -25,6 +25,14 @@ class ParamChecker {
             }
         }
     }
+
+    public dateFormat(date: string) {
+        const dateFormatRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+
+        if (!dateFormatRegex.test(date)) {
+            throw new Error('Date parameter should be in DD/MM/YYYY format');
+        }
+    }
 }
 
 export default new ParamChecker();
